5452973d6353aee8b58f15ecb64c389205f8cfcc
Murad Gereev <mrgereev@mail.ru>
2022-12-13 02:30:49 +0600

    lesson2 ended

---
+++ cmd/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+	"todo-app"
+	"todo-app/pkg/handler"
+)
+
+func main() {
+	handlers := new(handler.Handler)
+
+	srv := new(todo.Server)
+	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
+}
